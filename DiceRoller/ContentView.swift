5a5875238ab85a1008544f1fd3d9560b85a47ab1
Woo Sung Jahng <jlittle0919@icloud.com>
2024-08-28 18:59:59 +0900

    Initial commit

---
+++ DiceRoller/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Woo Sung Jahng on 2024/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+    
+    var body: some View {
+        VStack {
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+                .foregroundStyle(.white)
+            
+            HStack {
+                ForEach(1...numberOfDice, id: \.self) { _ in
+                    DiceView()
+                        .foregroundStyle(.white)
+                }
+            }
+            
+            HStack {
+                Button("Remove Dice", systemImage: "minus.circle.fill") {
+                    numberOfDice -= 1
+                }
+                .disabled(numberOfDice == 1)
+                
+                Button("Add Dice", systemImage: "plus.circle.fill") {
+                    numberOfDice += 1
+                }
+                .disabled(numberOfDice == 5)
+            }
+            .padding()
+            .labelStyle(.iconOnly)
+            .font(.title)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.appBackground)
+        .tint(.white)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Woo Sung Jahng on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
